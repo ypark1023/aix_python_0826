61ff4a7cd3018d56ddb95f66c3e1a31ec3e41c1b
pyw921023@gmail.com <pyw921023@gmail.com>
2026-08-26 12:37:39 +0900

    Python_Prac_Type3

--- P0826/p0826_04.py
+++ P0826/p0826_04.py
@@ -4,3 +4,20 @@ send1=int(input("송금액을 입력하시오"))
 print("잔액: {}".format(total1))
 print("송금액: {}".format(send1))
 print("총금액: {}".format(int(total1)+int(send1)))
+
+
+kor = int(input("국어 점수를 입력하시오"))
+eng = int(input("영어 점수를 입력하시오"))
+math = int(input("수학 점수를 입력하시오"))
+print("합계: {} 평균: {}".format(kor+eng+math, (kor+eng+math)/3))
+
+
+
+
+kor = int(input("국어 점수를 입력하시오"))
+eng = int(input("영어 점수를 입력하시오"))
+math = int(input("수학 점수를 입력하시오"))
+
+total = kor+eng+math
+avg = (kor+eng+math)/3
+print("합계: {} 평균: {}".format(total, avg))
