4e623f52221e7ba1711ea4e1a36d552486978447
pyw921023@gmail.com <pyw921023@gmail.com>
2026-08-26 10:22:07 +0900

    Python_lec0825

---
+++ P0825/p0825_04.py
@@ -0,0 +1,58 @@
+# 예약어는 변수로 쓸 수 없다
+
+a = 10
+b = 10.1
+c = "안녕"
+d = True #bool(C언어에선 boolean) - True 혹은 False
+
+
+
+# 10//5는 몫, 10%5는 나머지값, 10**5는 제곱값
+print(10+5)
+print(10-5)
+print(10*5)
+print(10/5)
+print(10//5)
+print(10%5)
+print(10**5)
+
+print(9+4)
+print(9-4)
+print(9*4)
+print(9/4)
+print(9//4)
+print(9%4)
+print(9**4)
+
+
+a = 10
+b = 5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+
+a = 9
+b = 4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+
+# 변수 타입을 확인하는 방법은 type(a)
+print(type(a))
+print(type(b))
+print(type(c))
+
+# <class 'int'>
+# <class 'float'>
+# <class 'str'>
+# <class 'bool'>
