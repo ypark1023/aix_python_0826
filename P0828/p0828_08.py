afdcee1e04abbc251fecf75310af2f0bc48c113c
pyw921023@gmail.com <pyw921023@gmail.com>
2026-08-28 17:59:46 +0900

    Python_lec0828_3

--- P0828/p0828_08.py
+++ P0828/p0828_08.py
@@ -33,3 +33,101 @@
 
 # for _ in range(3) :
 #     print("안녕")               # i 대신 _(언더바)를 써도 동일
+
+
+
+
+# for i in range(3) :
+#     no = i+1
+#     name = input("이름 입력: ")
+#     kor = input("국어 점수 입력: ")
+#     print("{},{},{}".format(no, name, kor))
+
+
+# for i in range(1, 10):
+#     print(f"2 X {i} = {2*i}")
+
+# 결과값은
+# 2 X 1 = 2
+# 2 X 2 = 4
+# 2 X 3 = 6
+# 2 X 4 = 8
+# 2 X 5 = 10
+# 2 X 6 = 12
+# 2 X 7 = 14
+# 2 X 8 = 16
+# 2 X 9 = 18
+
+
+# sum = 0
+# for i in range(1,11) :
+#     sum = sum+i
+# print("합계: ", sum)
+
+# sum = 0
+# for i in range(1,101) :
+#     sum = sum+i
+# print("합계: ", sum)
+
+# sum = 0
+# for i in range(1,1001) :
+#     sum = sum+i
+# print("합계: ", sum)
+
+# sum = 0
+# for i in range(1,10001) :
+#     sum = sum+i
+# print("합계: ", sum)
+
+# sum = 0
+# for i in range(1,100001) :
+#     sum = sum+i
+# print("합계: ", sum)
+
+# 결과값은
+# 합계:  55
+# 합계:  5050
+# 합계:  500500
+# 합계:  50005000
+# 합계:  5000050000
+
+
+
+# 퀴즈 : sum이 100이 넘는 때는 언제일까요?
+
+# sum = 0
+# for i in range(1,101) :
+#     sum = sum+i
+#     if sum > 100 :
+#         print("100보다 클 때: ", i)
+#         print("100초과될 때 시점: ", sum)
+#         break
+# print("합계: ", sum)
+
+
+
+# sum = 0
+# for i in range(1,11) :
+#     sum = sum+i
+#     if sum > 11 :
+#         print("100보다 클 때: ", i)
+#         print("100보다 바로 앞의 작은 수: ", sum-i)
+#         print("100초과될 때 시점: ", sum)
+
+
+
+
+# for i in range(0,10) :
+#     for j in range(0,10) :
+#         print("{}{}".format(i,j))
+
+# # 결과값은 00~99까지
+
+
+# # 번호표
+# for i in range(0,10) :
+#     for j in range(0,10) :
+#         for k in range (0,10) :
+#             print("[번호표]: {}{}{}".format(i,j,k))
+
+# # 결과값은 000~999까지
---
+++ P0828/p0829_09.py
@@ -0,0 +1,84 @@
+# 예제1 : 반복문을 사용해서 1~100까지 합을 출력
+# 예제2 : 200 넘는 시점의 i값과 i번째 합계를 출력
+# 예제3 : 200 넘는 이전 시점의 i값 합계를 출력
+# 예제4 : 구구단을 출력
+
+
+# # 예제 1 : 1~100 합계
+
+# sum = 0
+# for i in range(1,101) :
+#     sum = sum+i
+# print (sum)
+
+# # 결과값은 5050
+
+
+
+# # 예제 2~3 : 200 넘는 시점
+
+# sum = 0
+# for i in range(1,101) :
+#     sum = sum+i
+#     if sum > 200 : 
+#         print ("200 넘을 때 i값: ", i)
+#         print ("200 넘을 때 합계: ", sum+i)
+#         print ("200 넘기 전 i값: ", i-1)
+#         print ("200 넘기 전 합계: ", sum-i)
+#         break
+
+# # 결과값
+# # 200 넘을 때 i값:  20
+# # 200 넘을 때 합계:  230
+# # 200 넘기 전 i값:  19
+# # 200 넘기 전 합계:  190
+
+
+
+# # 예제 4: 구구단 출력
+# for i in range(1,10):
+#     for j in range(1,10):
+#         print("{} X {} = {}".format(i,j,i*j))
+
+# # 결과값
+# # 1 X 1 = 1 ~ 9 X 9 = 81
+
+
+
+# 예제 : 점수 입력하기
+
+name = []
+score = []
+for i in range(2) :
+    name.append(input("이름 입력: "))
+    score.append(input("점수 입력: "))
+
+for i in range(2) :
+    print("{} \t {}".format(name[i], score[i]))
+
+# 결과값
+    # 이름 입력: 홍길동 
+    # 점수 입력: 100
+    # 이름 입력: 유관순
+    # 점수 입력: 90
+    # 홍길동   100
+    # 유관순   90
+
+
+
+list_a = []
+for i in range(2) :
+    name = input("이름 입력: ")
+    score  = input("점수 입력: ")
+    list_a.append([name, score])
+
+for i in range(2) :
+    print("{}\t{}".format(*list_a[i]))
+
+# 결과값
+    # 이름 입력: 홍길동
+    # 점수 입력: 90
+    # 이름 입력: 유관순
+    # 점수 입력: 100
+    # 홍길동  90
+    # 유관순  100
